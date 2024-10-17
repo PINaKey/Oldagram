bfbf4f406de3e10e708112b1fd19e52e3032cb6e
Pinaki Bandyopadhyay <pinaki@Pinakis-MacBook-Air.local>
2024-10-17 11:42:59 +0530

    Oldagram v2.0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
     },
     {
         name: "Gustave Courbet",
@@ -15,16 +15,41 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
     },
-        {
+    {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+        comment:
+            "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+    },
+];
 
+const postsSectionEl = document.querySelector(".posts");
+let postsHTML = "";
+
+for (let i = 0; i < posts.length; i++) {
+    postsHTML += `
+    <div class="post">
+        <div class="profile-container">
+            <img class="post-avatar" src="${posts[i].avatar}" alt="Avatar of the user who shared the post." />
+            <p class="name">
+                ${posts[i].name}
+                <span class="location">${posts[i].location}</span>
+            </p>
+        </div>
+        <img class="post-img" src="${posts[i].post}" alt="An Oldagram post." />
+        <img id="like" class="icons" src="images/icon-heart.png" />
+        <img class="icons" src="images/icon-comment.png" />
+        <img class="icons" src="images/icon-dm.png" />
+        <p class="likes">${posts[i].likes} likes</p>
+        <p class="comment">${posts[i].username} <span>${posts[i].comment}</span></p>
+    </div>
+    `;
+}
+
+postsSectionEl.innerHTML = postsHTML;
